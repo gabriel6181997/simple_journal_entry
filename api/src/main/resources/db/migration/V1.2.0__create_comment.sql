30b2fd52ed8881bf21d70780065889d76659cca3
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-17 16:07:52 +0900

    Remove redundant comma in V1.2.0__create_comment.sql

--- api/src/main/resources/db/migration/V1.2.0__create_comment.sql
+++ api/src/main/resources/db/migration/V1.2.0__create_comment.sql
@@ -3,5 +3,5 @@ use simple_journal_entry_db;
 create table comments
 (
     id          bigint not null primary key auto_increment,
-    incurred_on DATE   not null,
+    incurred_on DATE   not null
 );
