7f2f18b350196de89ae6050aefbf162ce14189fd
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-22 14:05:50 +0900

    Fix bugs(WIP)

--- api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/query/CommentQuery.kt
+++ api/src/main/kotlin/com/okeicalm/simpleJournalEntry/handler/query/CommentQuery.kt
@@ -1,6 +1,6 @@
 package com.okeicalm.simpleJournalEntry.handler.query
 
-import com.expediagroup.graphql.spring.operations.Query
+import com.expediagroup.graphql.server.operations.Query
 import com.okeicalm.simpleJournalEntry.handler.type.CommentType
 import com.okeicalm.simpleJournalEntry.repository.CommentRepository
 import org.springframework.stereotype.Component
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Component
 class CommenetQuery(private val repository: CommentRepository) : Query {
     fun allComments(): List<CommentType> {
         val comments = repository.findAll()
-        return comments.map { CommentType(it) } 
+        return comments.map { CommentType(it) }
     }
 }
