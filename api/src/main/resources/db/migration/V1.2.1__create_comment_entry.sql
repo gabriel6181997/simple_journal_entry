4e71f3dab76e1f26a322baaed2c20e9c54db34fb
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-22 17:14:13 +0900

    Change table comment_entries content with reference to class commentEntry

--- api/src/main/resources/db/migration/V1.2.1__create_comment_entry.sql
+++ api/src/main/resources/db/migration/V1.2.1__create_comment_entry.sql
@@ -3,12 +3,9 @@ use simple_journal_entry_db;
 create table comment_entries
 (
     id          bigint not null primary key auto_increment,
-    comment_id  bigint not null,
+    text        text binary not null,
     journal_id  bigint not null,
-    side        tinyint not null,
     account_id  bigint not null,
-    value       int not null,
-    foreign key fk_comment (comment_id) references comments (id),
     foreign key fk_journal (journal_id) references journals (id),
     foreign key fk_account (account_id) references accounts (id)
 );
