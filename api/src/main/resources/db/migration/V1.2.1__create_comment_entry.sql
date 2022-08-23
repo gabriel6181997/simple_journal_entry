b6e60382c8d936e16457373bda5d36c67e29cc51
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-23 09:29:29 +0900

    Add foreign key comment_id to table comment_entries

--- api/src/main/resources/db/migration/V1.2.1__create_comment_entry.sql
+++ api/src/main/resources/db/migration/V1.2.1__create_comment_entry.sql
@@ -3,9 +3,11 @@ use simple_journal_entry_db;
 create table comment_entries
 (
     id          bigint not null primary key auto_increment,
+    comment_id  bigint not null,
     text        text binary not null,
     journal_id  bigint not null,
     account_id  bigint not null,
     foreign key fk_journal (journal_id) references journals (id),
-    foreign key fk_account (account_id) references accounts (id)
+    foreign key fk_account (account_id) references accounts (id),
+    foreign key fk_comment (comment_id) references comments (id)
 );
